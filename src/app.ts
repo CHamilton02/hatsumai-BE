f68ddd5d958ca2ec91ed904c4d6060545b71e541
Camdyn Hamilton <c02hamil@gmail.com>
2025-07-07 15:14:09 -0400

    feat: add cors library

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import user from './routes/user'
 import health from './routes/health'
 import project from './routes/project'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/health', health)
 app.use('/user', user)
